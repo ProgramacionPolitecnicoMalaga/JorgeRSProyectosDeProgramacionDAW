0a48e9f3b03c4eb2b1559c4ba284d1300dd42912
Jorge Rios Sanchez <jorge.rios.sanchez@gmail.com>
2019-10-30 09:57:47 +0100

    subida act13 excepciones cola

---
+++ Act13ExcepcionesCola/src/ConsumidorCola.java
@@ -0,0 +1,20 @@
+public class ConsumidorCola {
+    private static Cola cola;
+    private static final int TAM_COLA = 5;
+
+    public static void main(String[] args) {
+        try {
+        cola = new Cola(TAM_COLA);
+        cola.meter(10);
+        System.out.println(cola);
+        cola.sacar();
+        System.out.println(cola);
+        cola.sacar();
+        System.out.println(cola);
+
+        }catch (ExcepcionColaVacia e){
+            System.out.println(e.advertenciaExcepcion());
+        }
+
+    }
+}
